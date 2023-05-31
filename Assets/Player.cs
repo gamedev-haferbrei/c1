b1298da8bb0c071feabaaab0af075449307eb248
fcaddr <github@caddr.de>
2023-05-31 15:25:08 +0200

    player movement

---
+++ Assets/Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] InputAction playerControls;
+
+    Vector2 moveDirection;
+    Rigidbody2D rb;
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = playerControls.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = moveDirection * moveSpeed;
+    }
+}
