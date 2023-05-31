a8dfe7c2ad8eeb90045fd05bc18aa078ce1cc6e6
fcaddr <github@caddr.de>
2023-05-31 12:26:06 +0200

    added menu (for some reason the play button isnt receiving events though???

---
+++ Assets/HighscoreDisplay.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HighscoreButton : MonoBehaviour
+{
+    TextMeshProUGUI text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = $"Highscore: {GetHighscore()}";
+    }
+
+    int GetHighscore() => PlayerPrefs.HasKey("Highscore") ? PlayerPrefs.GetInt("Highscore") : 0;
+}
