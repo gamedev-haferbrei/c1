2fa72d49a477e82c4bd30b3816fad1f8b2819b36
fcaddr <github@caddr.de>
2023-05-31 16:09:57 +0200

    add UI for special attacks

---
+++ Assets/SpecialAttackDisplay.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class SpecialAttackDisplay : MonoBehaviour
+{
+    [SerializeField] Image fill;
+    [SerializeField] InputAction attackControls;
+    [SerializeField] float cooldownLength = 1f;
+
+    float cooldownTimer = 0f;
+
+    private void OnEnable()
+    {
+        attackControls.Enable();
+        attackControls.performed += DoAttack;
+    }
+
+    private void OnDisable()
+    {
+        attackControls.Disable();
+    }
+
+    void DoAttack(InputAction.CallbackContext ctx)
+    {
+        if (cooldownTimer > 0f) return;
+        cooldownTimer = 1f;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cooldownTimer = Mathf.Max(0f, cooldownTimer - Time.deltaTime);
+        fill.fillAmount = cooldownTimer / cooldownLength;
+    }
+}
