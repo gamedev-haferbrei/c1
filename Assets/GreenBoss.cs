1f4d972c01ecc4c9f4f890dff9ea439f91679895
fcaddr <github@caddr.de>
2023-06-23 12:05:24 +0200

    finished green boss

--- Assets/GreenBoss.cs
+++ Assets/GreenBoss.cs
@@ -9,7 +9,7 @@ public class GreenBoss : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(StarBurst());
+        StartCoroutine(AttackLoop());
     }
 
     // Update is called once per frame
@@ -17,19 +17,30 @@ public class GreenBoss : MonoBehaviour
     {
     }
 
+    IEnumerator AttackLoop()
+    {
+        while (true)
+        {
+            yield return StartCoroutine(StarBurst());
+            yield return new WaitForSeconds(1f);
+            yield return StartCoroutine(SinePattern());
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    Vector3[] cannonPositions = new Vector3[]
+    {
+        new Vector3(-1.4f, -2f),
+        new Vector3(-1.4f, +2f),
+    };
+
     IEnumerator StarBurst()
     {
         for (int burst = 0; burst < 3; burst++)
         {
-            Vector3[] cannonPositions = new Vector3[]
-            {
-                new Vector3(-0.6f, -1f),
-                new Vector3(-0.6f, +1f),
-            };
-
             foreach (Vector3 cannonPosition in cannonPositions)
             {
-                int anglesN = 6 + burst;
+                int anglesN = 3 + burst;
                 float spanAngle = 74;
                 for (int angle = 0; angle < anglesN; angle++)
                 {
@@ -39,5 +50,39 @@ public class GreenBoss : MonoBehaviour
 
             yield return new WaitForSeconds(0.7f);
         }
+    }
+
+    IEnumerator SinePattern()
+    {
+        float totalTimeElapsed = 0;
+        float timeSinceLastStar = 0;
+
+        const float totalSineTime = 8f;
+        const float timeBetweenStars = 0.3f;
+
+        Vector3 origin = transform.position;
+
+        while (true)
+        {
+            yield return null;
+
+            totalTimeElapsed += Time.deltaTime;
+            timeSinceLastStar += Time.deltaTime;
+
+            transform.position = origin + new Vector3(0, 4 * Mathf.Sin(totalTimeElapsed / totalSineTime * 3 * Mathf.PI));
+
+            if (totalTimeElapsed > totalSineTime) break;
+            if (timeSinceLastStar < timeBetweenStars) continue;    
+            
+            timeSinceLastStar -= timeBetweenStars;
+
+            foreach (Vector3 cannonPosition in cannonPositions)
+            {
+                Instantiate(starProjectilePrefab, transform.position + cannonPosition, Quaternion.identity);
+            }
+
+        }
+
+        transform.position = origin;
     }
 }
